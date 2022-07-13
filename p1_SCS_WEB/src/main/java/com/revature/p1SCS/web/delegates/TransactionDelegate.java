828448849a2916f930bfa214405630fb2e6dae47
Barry Norton <barrynorton107@gmail.com>
2022-07-13 15:25:00 -0400

    added p1_SCS_ORM as a dependency

---
+++ p1_SCS_WEB/src/main/java/com/revature/p1SCS/web/delegates/TransactionDelegate.java
@@ -0,0 +1,14 @@
+package com.revature.p1SCS.web.delegates;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class TransactionDelegate implements ServletDelegate{
+
+	@Override
+	public void handle(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- p1_SCS_WEB/src/main/java/com/revature/p1SCS/web/delegates/UserDelegate.java
+++ p1_SCS_WEB/src/main/java/com/revature/p1SCS/web/delegates/UserDelegate.java
@@ -1,13 +1,37 @@
 package com.revature.p1SCS.web.delegates;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.p1SCS.orm.models.Query;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class UserDelegate implements ServletDelegate{
+	private Query in = new Query();
+	private ObjectMapper objMapper = new ObjectMapper();
+	private List<Query> getResult = new ArrayList<>();
 
 	@Override
 	public void handle(HttpServletRequest req, HttpServletResponse resp) {
-		// TODO Auto-generated method stub
+		try {
+			Query in = objMapper.readValue(req.getInputStream(), Query.class);
+			switch(req.getMethod()) {
+			case "GET":
+				break;
+			case "POST":
+				break;
+			case "PUT":
+				break;
+			case "DELETE":
+				break;
+			}
+		}
+		catch (Exception e) {
+			//TODO Exception Logger
+		}
 		
 	}
 
