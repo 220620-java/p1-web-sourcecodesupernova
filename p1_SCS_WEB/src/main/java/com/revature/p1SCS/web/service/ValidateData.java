a684f6828a58e7b8d8632e19ee818050972f9794
Barry Norton <barrynorton107@gmail.com>
2022-07-21 13:03:16 -0400

    Added documentation to Delegates and ValidateData

--- p1_SCS_WEB/src/main/java/com/revature/p1SCS/web/service/ValidateData.java
+++ p1_SCS_WEB/src/main/java/com/revature/p1SCS/web/service/ValidateData.java
@@ -3,8 +3,12 @@ package com.revature.p1SCS.web.service;
 import java.sql.Date;
 
 public class ValidateData {
-	/*Data with the char type should have the same length as a given size
-	 * Also, if the input contains a ';' it would break the SQL */
+	/**Data with the char type should have the same length as a given size
+	 * Also, if the input contains a ';' it would break the SQL 
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @param size (int): an integer representing the size of the data type
+	 * @return A Boolean representing the input's validity*/
 	public Boolean charType(String input, int size) {
 		Boolean valid = true;
 		if (input.contains(";") || input.length() != size) {
@@ -13,8 +17,12 @@ public class ValidateData {
 		return valid;
 	}
 	
-	/*Data with the varchar type should have a length less than or equal to a given size
-	 * Also, if the input contains a ';' it would break the SQL*/
+	/**Data with the varchar type should have a length less than or equal to a given size
+	 * Also, if the input contains a ';' it would break the SQL
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @param size (int): an integer representing the size of the data type
+	 * @return A Boolean representing the input's validity*/
 	public Boolean varcharType(String input, int size) {
 		Boolean valid = true;
 		if (input.contains(";") || input.length() > size) {
@@ -23,8 +31,11 @@ public class ValidateData {
 		return valid;
 	}
 	
-	/*Data with the text type has no set size limit
-	 * But if the input contains a ';' it would still break the SQL*/
+	/**Data with the text type has no set size limit
+	 * But if the input contains a ';' it would still break the SQL
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @return A Boolean representing the input's validity*/
 	public Boolean textType(String input) {
 		Boolean valid = true;
 		if (input.contains(";")) {
@@ -33,7 +44,10 @@ public class ValidateData {
 		return valid;
 	}
 	
-	/*Data with the integer type should be able to parse into an integer*/
+	/**Data with the integer type should be able to parse into an integer
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @return A Boolean representing the input's validity*/
 	public Boolean integerType(String input) {
 		Boolean valid = true;
 		try {
@@ -46,7 +60,10 @@ public class ValidateData {
 		return valid;
 	}
 	
-	/*Data with the double type should be able to parse into a double*/
+	/**Data with the double type should be able to parse into a double
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @return A Boolean representing the input's validity*/
 	public Boolean decimalType(String input) {
 		Boolean valid = true;
 		try {
@@ -59,7 +76,10 @@ public class ValidateData {
 		return valid;
 	}
 	
-	/*Data with the double type should be able to parse into a double*/
+	/**Data with the double type should be able to parse into a double
+	 * 
+	 * @param input (String): a string to be checked for validation
+	 * @return A Boolean representing the input's validity*/
 	public Boolean dateType(String input) {
 		Boolean valid = true;
 		try {
